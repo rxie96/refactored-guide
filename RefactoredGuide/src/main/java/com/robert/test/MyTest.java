070d374169ad06459d06ba85c27f6dfa7922710e
Robert Xie <rxie96@gmail.com>
2020-03-05 13:41:37 -0500

    pushing

---
+++ RefactoredGuide/src/main/java/com/robert/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.robert.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
+
+}
